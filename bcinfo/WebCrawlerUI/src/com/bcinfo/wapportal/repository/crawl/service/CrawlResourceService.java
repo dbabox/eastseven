e593b91b881d0533bd5d589eacc314fa8756cdae
eastseven <eastseven@foxmail.com>
2009-10-20 11:59:03 +0000

    新增爬取资源服务类，目前只实现了一个发送资源的方法

---
+++ bcinfo/WebCrawlerUI/src/com/bcinfo/wapportal/repository/crawl/service/CrawlResourceService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.bcinfo.wapportal.repository.crawl.service;
+
+/**
+ * @author dongq
+ * 
+ *         create time : 2009-10-20 обнГ04:42:11
+ */
+public interface CrawlResourceService {
+
+	public Boolean sendResource(String channelId, String[] resourceIds);
+}
