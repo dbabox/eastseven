9199f346f3ea03254e9e9c7828112e21e0231d97
eastseven <eastseven@foxmail.com>
2009-10-20 11:48:51 +0000

    新增了保存图片地址的字段

--- bcinfo/WebCrawler/src/com/bcinfo/wapportal/repository/crawl/domain/bo/FolderBO.java
+++ bcinfo/WebCrawler/src/com/bcinfo/wapportal/repository/crawl/domain/bo/FolderBO.java
@@ -16,22 +16,24 @@ public class FolderBO implements Serializable {
 
 	private static final long serialVersionUID = -6434622155462613619L;
 
-	private String folderId;//channel_id
+	private String folderId;// channel_id
 	private String title;
 	private String link;
 	private String content;
+	private String imgPathSet;
 	private List<ResourceBO> resources;
 
 	public FolderBO() {
 
 	}
-	
-	public FolderBO(String folderId, String title, String link, String content) {
+
+	public FolderBO(String folderId, String title, String link, String content, String imgPathSet) {
 		super();
 		this.folderId = folderId;
 		this.title = title;
 		this.link = link;
 		this.content = content;
+		this.imgPathSet = imgPathSet;
 	}
 
 	public String getFolderId() {
@@ -74,11 +76,19 @@ public class FolderBO implements Serializable {
 		this.resources = resources;
 	}
 
+	public String getImgPathSet() {
+		return imgPathSet;
+	}
+
+	public void setImgPathSet(String imgPathSet) {
+		this.imgPathSet = imgPathSet;
+	}
+
 	@Override
 	public String toString() {
 		return "FolderBO [content=" + content + ", folderId=" + folderId
-				+ ", link=" + link + ", resources=" + resources + ", title="
-				+ title + "]";
+				+ ", imgPathSet=" + imgPathSet + ", link=" + link
+				+ ", resources=" + resources + ", title=" + title + "]";
 	}
 
 }
