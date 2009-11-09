909e904fcb93271871bb58daea86806a48b3a6ef
eastseven@foxmail.com <eastseven@foxmail.com@c899bf3c-8e1a-11de-a7db-6fd96749a44a>
2009-11-09 03:41:37 +0000

    添加每天生成一个日志文件的功能； 修改读取配置文件的方法。

--- bcinfo/WebCrawler/src/com/bcinfo/wapportal/repository/crawl/file/ConfigPropertyUtil.java
+++ bcinfo/WebCrawler/src/com/bcinfo/wapportal/repository/crawl/file/ConfigPropertyUtil.java
@@ -4,6 +4,7 @@
 package com.bcinfo.wapportal.repository.crawl.file;
 
 import java.io.BufferedInputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.util.Properties;
@@ -15,21 +16,56 @@ import java.util.Properties;
  */
 public class ConfigPropertyUtil {
 
+	public static Properties getLog4jProperty(){
+		Properties property = null;
+		InputStream in = null;
+		try{
+			//取得当前工程所在的根目录
+			String userDir = System.getProperty("user.dir");
+			if(userDir != null && !"".equals(userDir)){
+				File configFolder = new File(userDir+"/config");
+				if(configFolder.exists()){
+					//生产环境,文件放在config目录下,Windows or Linux
+					in = new BufferedInputStream(new FileInputStream(userDir+"/config/log4j.properties"));
+				}else{
+					//开发环境,文件放在src目录下
+					in = new BufferedInputStream(new FileInputStream(userDir+"/src/log4j.properties"));
+				}
+			}else{
+				//未找到工程路径
+				System.out.println("未找到工程路径,无法加载log4j.properties文件");
+			}
+			property = new Properties();
+			property.load(in);
+			in.close();
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return property;
+	}
+	
 	public Properties getConfigProperty(){
 		Properties property = null;
 		InputStream in = null;
 		try{
-			String os = System.getenv("OS");
-			if(os!=null){
-				in = new BufferedInputStream (new FileInputStream("src/config.properties"));
-			}else{
-				in = new BufferedInputStream (new FileInputStream("/usr/local/oracle/apache-tomcat-6.0.18/webcrawler/config/config.properties"));
-				if(in != null){
-					System.out.println(" 成功读取/usr/local/oracle/apache-tomcat-6.0.18/webcrawler/config/config.properties ");
+			//取得当前工程所在的根目录
+			String userDir = System.getProperty("user.dir");
+			
+			if(userDir != null && !"".equals(userDir)){
+				File configFolder = new File(userDir+"/config");
+				if(configFolder.exists()){
+					//生产环境,Windows or Linux
+					in = new BufferedInputStream(new FileInputStream(userDir+"/config/config.properties"));
 				}else{
-					System.out.println(" 读取/usr/local/oracle/apache-tomcat-6.0.18/webcrawler/config/config.properties失败 ");
+					//开发环境
+					in = new BufferedInputStream(new FileInputStream(userDir+"/src/config.properties"));
 				}
+			}else{
+				//未找到工程路径
+				System.out.println("未找到工程路径,无法加载config.properties文件");
 			}
+			
 			property = new Properties();
 			property.load(in);
 			in.close();
