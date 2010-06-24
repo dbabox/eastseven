c900de4a9a9d9cd7c6aa93697f72d559f27c1ef7
eastseven <eastseven@foxmail.com>
2010-06-24 09:04:43 +0000

    对于所有抓取Bundle，改用Quartz调度框架控制抓取频率

--- bcinfo/WebCrawlerOSGi/com.bcinfo.crawl.channel.mobile/src/com/bcinfo/crawl/channel/mobile/Activator.java
+++ bcinfo/WebCrawlerOSGi/com.bcinfo.crawl.channel.mobile/src/com/bcinfo/crawl/channel/mobile/Activator.java
@@ -1,15 +1,18 @@
 package com.bcinfo.crawl.channel.mobile;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SimpleTrigger;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
 
 import com.bcinfo.crawl.dao.log.service.CrawlerLogService;
 import com.bcinfo.crawl.dao.service.WebCrawlerDao;
@@ -20,82 +23,10 @@ public class Activator implements BundleActivator {
 
 	private static final Log log = LogFactory.getLog(Activator.class);
 	
-	private ScheduledExecutorService scheduled = null;
+	private Scheduler scheduler = null;
 	
 	public List<Site> getSites() {
 		List<Site> sites = new ArrayList<Site>();
-		/*
-		Site site = null;
-		site = new Site();
-		site.setChannelId(282L);
-		site.setChannelName("手机新闻");
-		site.setUrl("http://digi.tech.qq.com/l/mobile/02new/list20100329144655.htm");
-		site.setPageSuffix("digi.*?/\\d{4}\\d{2}\\d{2}/\\d+.htm");
-		site.setDatePattern("yyyyMMdd");
-		site.setCharset("GB2312");
-		site.setDebug(false);
-		//site.setRealTime(false);
-		site.setPageSelector("div[id=ArtPLink]>ul>li>a");
-		site.setContentSelector("div[id=Cnt-Main-Article-QQ]");
-		site.setName(Site.QQ);
-		sites.add(site);
-		
-		site = new Site();
-		site.setChannelId(283L);
-		site.setChannelName("手机行情");
-		site.setUrl("http://tech.sina.com.cn/mobile/market/qbgd/roll/index.html");
-		site.setPageSuffix("tech.*?mobile.*?\\d{4}-\\d{2}-\\d{2}/\\d+.shtml");
-		site.setDatePattern("yyyy-MM-dd");
-		site.setCharset("GB2312");
-		site.setDebug(false);
-		//site.setRealTime(false);
-		site.setPageSelector("span[id=_function_code_page]>p>a");
-		site.setContentSelector("div[id=artibody]>p");
-		site.setName(Site.SINA);
-		sites.add(site);
-		
-		site = new Site();
-		site.setChannelId(283L);
-		site.setChannelName("手机导购");
-		site.setUrl("http://digi.tech.qq.com/l/mobile/02show/list20100329164413.htm");
-		site.setPageSuffix("digi.*?/\\d{4}\\d{2}\\d{2}/\\d+.htm");
-		site.setDatePattern("yyyyMMdd");
-		site.setCharset("GB2312");
-		site.setDebug(false);
-		//site.setRealTime(false);
-		site.setPageSelector("div[id=ArtPLink]>ul>li>a");
-		site.setContentSelector("div[id=Cnt-Main-Article-QQ]");
-		site.setName(Site.QQ);
-		sites.add(site);
-		
-		site = new Site();
-		site.setChannelId(285L);
-		site.setChannelName("手机测评");
-		site.setUrl("http://tech.sina.com.cn/mobile/preview/roll/index.html");
-		site.setPageSuffix("tech.*?mobile.*?\\d{4}-\\d{2}-\\d{2}/\\d+.shtml");
-		site.setDatePattern("yyyy-MM-dd");
-		site.setCharset("GB2312");
-		site.setDebug(false);
-		//site.setRealTime(false);
-		site.setPageSelector("span[id=_function_code_page]>p>a");
-		site.setContentSelector("div[id=artibody]>p");
-		site.setName(Site.SINA);
-		sites.add(site);
-		
-		site = new Site();
-		site.setChannelId(286L);
-		site.setChannelName("手机图赏");
-		site.setUrl("http://tech.sina.com.cn/mobile/photos/NOKIA.html");
-		site.setPageSuffix("tech.*?mobile.*?\\d{4}-\\d{2}-\\d{2}/\\d+.shtml");
-		site.setDatePattern("yyyy-MM-dd");
-		site.setCharset("GB2312");
-		site.setDebug(false);
-		//site.setRealTime(false);
-		site.setPageSelector("span[id=_function_code_page]>p>a");
-		site.setContentSelector("div[id=artibody]>p");
-		site.setName(Site.SINA);
-		sites.add(site);
-		*/
 		String configPath = "conf/com.bcinfo.crawl.channel.mobile.xml";
 		sites.addAll(new SiteUtil().getSites(configPath));
 		return sites;
@@ -103,26 +34,27 @@ public class Activator implements BundleActivator {
 	
 	public void start(BundleContext context) throws Exception {
 		log.info("手机频道抓取程序启动");
-		
-		int corePoolSize = getSites().size();
-		scheduled = Executors.newScheduledThreadPool(corePoolSize);
-		
 		CrawlerLogService crawlerLogService = (CrawlerLogService)context.getService(context.getServiceReference(CrawlerLogService.class.getName()));
 		WebCrawlerDao webCrawlerDao = (WebCrawlerDao)context.getService(context.getServiceReference(WebCrawlerDao.class.getName()));
+		scheduler = StdSchedulerFactory.getDefaultScheduler();
+		scheduler.start();
 		int index = 1;
 		for(Site site : getSites()) {
-			SiteParser siteParser = new SiteParser();
-			siteParser.setSite(site);
-			siteParser.setWebCrawlerDao(webCrawlerDao);
-			siteParser.setCrawlerLogService(crawlerLogService);
-			scheduled.scheduleAtFixedRate(siteParser, index, site.getFrequency(), TimeUnit.SECONDS);
+			String name = site.getChannelId()+"."+site.getChannelName()+"."+index;
+			String group = "手机";
+			JobDetail job = new JobDetail(name, group, SiteParser.class);
+			job.getJobDataMap().put("site", site);
+			job.getJobDataMap().put("webCrawlerDao", webCrawlerDao);
+			job.getJobDataMap().put("crawlerLogService", crawlerLogService);
+			Trigger trigger = new SimpleTrigger(name, group, new Date(System.currentTimeMillis()+index*1000), null, -1, site.getFrequency());
+			scheduler.scheduleJob(job, trigger);
 			index++;
 		}
 		
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		if(scheduled != null) scheduled.shutdown();
+		if(scheduler != null) scheduler.shutdown(true);
 		log.info("手机频道抓取程序关闭");
 	}
 
--- bcinfo/WebCrawlerOSGi/com.bcinfo.crawl.channel.news.sdinfo/src/com/bcinfo/crawl/channel/news/sdinfo/Activator.java
+++ bcinfo/WebCrawlerOSGi/com.bcinfo.crawl.channel.news.sdinfo/src/com/bcinfo/crawl/channel/news/sdinfo/Activator.java
@@ -1,15 +1,18 @@
 package com.bcinfo.crawl.channel.news.sdinfo;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SimpleTrigger;
+import org.quartz.Trigger;
+import org.quartz.impl.StdSchedulerFactory;
 
 import com.bcinfo.crawl.dao.log.service.CrawlerLogService;
 import com.bcinfo.crawl.dao.service.WebCrawlerDao;
@@ -20,73 +23,10 @@ public class Activator implements BundleActivator {
 
 	private static final Log log = LogFactory.getLog(Activator.class);
 
-	private ScheduledExecutorService scheduled = null;
-
+	private Scheduler scheduler = null;
+	
 	public List<Site> getSites() {
 		List<Site> sites = new ArrayList<Site>();
-		/*
-		Site site = null;
-		
-		site = new Site();
-		site.setChannelId(981L);
-		site.setChannelName("山东新闻");
-		site.setUrl("http://news.sdinfo.net/sdxw/sdyw/default.shtml");
-		site.setPageSuffix("sdyw/\\d+.shtml");
-		site.setDatePattern("yyyy-MM-dd");
-		site.setCharset("GB2312");
-		site.setDebug(false);
-		site.setRealTime(true);
-		site.setDeployTimeSelector("div[class=title]>span");
-		site.setPageSelector("div[id=pager]>ul>li>a");
-		site.setContentSelector("div[id=tbcontent]>p");
-		site.setName(Site.SD_INFO);
-		sites.add(site);
-		
-		site = new Site();
-		site.setChannelId(982L);
-		site.setChannelName("济南新闻");
-		site.setUrl("http://news.sdinfo.net/sdxw/jnxw/default.shtml");
-		site.setPageSuffix("jnxw/\\d+.shtml");
-		site.setDatePattern("yyyy-MM-dd");
-		site.setCharset("GB2312");
-		site.setDebug(false);
-		site.setRealTime(true);
-		site.setDeployTimeSelector("div[class=title]>span");
-		site.setPageSelector("div[id=pager]>ul>li>a");
-		site.setContentSelector("div[id=tbcontent]>p");
-		site.setName(Site.SD_INFO);
-		sites.add(site);
-		
-		site = new Site();
-		site.setChannelId(983L);
-		site.setChannelName("青岛新闻");
-		site.setUrl("http://news.sdinfo.net/sdxw/qdxw/default.shtml");
-		site.setPageSuffix("qdxw/\\d+.shtml");
-		site.setDatePattern("yyyy-MM-dd");
-		site.setCharset("GB2312");
-		site.setDebug(false);
-		site.setRealTime(true);
-		site.setDeployTimeSelector("div[class=title]>span");
-		site.setPageSelector("div[id=pager]>ul>li>a");
-		site.setContentSelector("div[id=tbcontent]>p");
-		site.setName(Site.SD_INFO);
-		sites.add(site);
-		
-		site = new Site();
-		site.setChannelId(1082L);
-		site.setChannelName("山东各地");
-		site.setUrl("http://news.sdinfo.net/sdxw/gdxw/default.shtml");
-		site.setPageSuffix("gdxw/\\d+.shtml");
-		site.setDatePattern("yyyy-MM-dd");
-		site.setCharset("GB2312");
-		site.setDebug(false);
-		site.setRealTime(true);
-		site.setDeployTimeSelector("div[class=title]>span");
-		site.setPageSelector("div[id=pager]>ul>li>a");
-		site.setContentSelector("div[id=tbcontent]>p");
-		site.setName(Site.SD_INFO);
-		sites.add(site);
-		*/
 		String configPath = "conf/com.bcinfo.crawl.channel.news.sdinfo.xml";
 		sites.addAll(new SiteUtil().getSites(configPath));
 		return sites;
@@ -94,26 +34,28 @@ public class Activator implements BundleActivator {
 	
 	public void start(BundleContext context) throws Exception {
 		log.info("新闻频道-山东-抓取程序启动");
-		
-		int corePoolSize = getSites().size();
-		scheduled = Executors.newScheduledThreadPool(corePoolSize);
-		
 		CrawlerLogService crawlerLogService = (CrawlerLogService)context.getService(context.getServiceReference(CrawlerLogService.class.getName()));
 		WebCrawlerDao webCrawlerDao = (WebCrawlerDao)context.getService(context.getServiceReference(WebCrawlerDao.class.getName()));
+		
+		scheduler = StdSchedulerFactory.getDefaultScheduler();
+		scheduler.start();
 		int index = 1;
 		for(Site site : getSites()) {
-			SiteParser siteParser = new SiteParser();
-			siteParser.setSite(site);
-			siteParser.setWebCrawlerDao(webCrawlerDao);
-			siteParser.setCrawlerLogService(crawlerLogService);
-			scheduled.scheduleAtFixedRate(siteParser, index, site.getFrequency(), TimeUnit.SECONDS);
+			String name = site.getChannelId()+"."+site.getChannelName()+"."+index;
+			String group = "山东";
+			JobDetail job = new JobDetail(name, group, SiteParser.class);
+			job.getJobDataMap().put("site", site);
+			job.getJobDataMap().put("webCrawlerDao", webCrawlerDao);
+			job.getJobDataMap().put("crawlerLogService", crawlerLogService);
+			Trigger trigger = new SimpleTrigger(name, group, new Date(System.currentTimeMillis()+index*1000), null, -1, site.getFrequency());
+			scheduler.scheduleJob(job, trigger);
 			index++;
 		}
 		
 	}
 
 	public void stop(BundleContext context) throws Exception {
-		if(scheduled != null) scheduled.shutdown();
+		if(scheduler != null) scheduler.shutdown(true);
 		log.info("新闻频道-山东-抓取程序关闭");
 	}
 
