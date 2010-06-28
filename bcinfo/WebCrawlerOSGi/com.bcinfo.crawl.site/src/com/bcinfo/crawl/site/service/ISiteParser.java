2222d6feb92ff16e86f37ccaa4c17c1f995ea7d6
eastseven <eastseven@foxmail.com>
2010-06-28 04:49:21 +0000

    更新抽象模板类AbstractSiteParser，增加hook方法、format方法及图片标签抽取方法

--- bcinfo/WebCrawlerOSGi/com.bcinfo.crawl.site/src/com/bcinfo/crawl/site/service/ISiteParser.java
+++ bcinfo/WebCrawlerOSGi/com.bcinfo.crawl.site/src/com/bcinfo/crawl/site/service/ISiteParser.java
@@ -3,7 +3,10 @@
  */
 package com.bcinfo.crawl.site.service;
 
-import java.util.List;
+import com.bcinfo.crawl.dao.log.service.CrawlerLogService;
+import com.bcinfo.crawl.dao.service.WebCrawlerDao;
+import com.bcinfo.crawl.domain.model.Resource;
+import com.bcinfo.crawl.domain.model.Site;
 
 /**
  * @author dongq
@@ -12,9 +15,13 @@ import java.util.List;
  */
 public interface ISiteParser extends Runnable {
 
-	public void crawlSiteLinks();
+	public void crawlSiteStart();
 	
-	public void crawlPageContent();
+	public void crawlPageContent(Resource resource);
 	
-	public List<String> getPageLinks(String pageLink, String pageEncoding);
+	public void setCrawlerLogService(CrawlerLogService crawlerLogService);
+
+	public void setWebCrawlerDao(WebCrawlerDao webCrawlerDao);
+
+	public void setSite(Site site);
 }
