36d275df775415bdf15fdc337a0e89947cd16cf7
eastseven.dongq <eastseven.dongq@gmail.com>
2012-03-13 10:09:11 +0000

    开放式问卷

---
+++ personal/analytics/src/main/java/org/dongq/analytics/repository/QuestionnaireTemplateRepository.java
@@ -0,0 +1,8 @@
+package org.dongq.analytics.repository;
+
+import org.dongq.analytics.model.domain.QuestionnaireTemplate;
+import org.springframework.data.repository.CrudRepository;
+
+public interface QuestionnaireTemplateRepository extends CrudRepository<QuestionnaireTemplate, Long> {
+
+}
